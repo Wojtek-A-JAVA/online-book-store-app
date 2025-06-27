0f9499413f607e5b80cd7ccbcc39787291e767d1
Wojtek-A <wojtek.a@hotmail.com>
2025-06-27 15:53:39 +0200

    Spring framework to spring boot migration, DTO, mapper, BookControlller, BookService, BookRepository, EntityNotFoundException.

--- src/main/java/book/store/repository/impl/BookRepositoryImpl.java
+++ src/main/java/book/store/repository/impl/BookRepositoryImpl.java
@@ -2,50 +2,51 @@ package book.store.repository.impl;
 
 import book.store.model.Book;
 import book.store.repository.BookRepository;
-import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@RequiredArgsConstructor
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
 
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    private final EntityManagerFactory entityManagerFactory;
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
+        EntityTransaction transaction = null;
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            entityManager.persist(book);
             transaction.commit();
+            return book;
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
             throw new RuntimeException("Can't add book " + book + " to DB", e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
-        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            Book book = entityManager.find(Book.class, id);
+            return book != null ? Optional.of(book) : Optional.empty();
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find book", e);
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            CriteriaQuery<Book> criteriaQuery = session.getCriteriaBuilder()
-                    .createQuery(Book.class);
-            criteriaQuery.from(Book.class);
-            return session.createQuery(criteriaQuery).getResultList();
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("select b from Book b", Book.class).getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Can't find books", e);
         }
--- src/main/java/book/store/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/service/impl/BookServiceImpl.java
@@ -1,25 +1,42 @@
 package book.store.service.impl;
 
+import book.store.dto.BookDto;
+import book.store.dto.CreateBookRequestDto;
+import book.store.exception.EntityNotFoundException;
+import book.store.mapper.BookMapper;
 import book.store.model.Book;
 import book.store.repository.BookRepository;
 import book.store.service.BookService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
 
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto bookDto) {
+        Book book = bookMapper.toBook(bookDto);
+        Book savedBook = bookRepository.save(book);
+        return bookMapper.toBookDto(savedBook);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Book with id " + id + " not found"));
+        return bookMapper.toBookDto(book);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toBookDto)
+                .collect(Collectors.toList());
     }
 }
