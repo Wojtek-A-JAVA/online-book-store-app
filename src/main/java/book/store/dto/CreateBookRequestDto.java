b8769dd00fafdac2f356eaea740a9f2d9056109b
Wojtek-A <wojtek.a@hotmail.com>
2025-07-29 15:01:59 +0200

    Spring Boot Portfolio Project — Part 4. Added CustomGlobalExceptionHandler class.

--- src/main/java/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/book/store/dto/CreateBookRequestDto.java
@@ -1,15 +1,23 @@
 package book.store.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     private Long id;
+    @NotNull(message = "Title cannot be null")
     private String title;
+    @NotNull(message = "Author cannot be null")
     private String author;
+    @NotNull(message = "ISBN cannot be null, must be unique 13-digit number")
+    @Size(min = 13, max = 13)
     private String isbn;
-
+    @NotNull(message = "Price cannot be null")
+    @DecimalMin(value = "0.01", message = "Price must be greater than zero")
     private BigDecimal price;
     private String description;
     private String coverImage;
