cd940f194652cac90afb1c526e3ba761c9d5914d
Wojtek-A <wojtek.a@hotmail.com>
2025-08-01 20:53:55 +0200

    Spring Boot Portfolio Project — Part 5. Added Pagination, Sorting, Swagger

--- src/main/java/book/store/service/impl/BookServiceImpl.java
+++ src/main/java/book/store/service/impl/BookServiceImpl.java
@@ -12,6 +12,7 @@ import book.store.service.BookService;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -38,8 +39,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toBookDto)
                 .collect(Collectors.toList());
     }
